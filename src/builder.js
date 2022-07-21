e08b3a2e19f7db1a7f6bf9dd59fd6d45af20b480
Valentina <v.vasileva@nuzhnapomosh.ru>
2022-07-21 18:12:48 +0300

    small fixes

--- src/builder.js
+++ src/builder.js
@@ -8,7 +8,9 @@ const createNode = (key, type, oldValue, newValue) => ({
 });
 
 const buildDiff = (object1, object2) => {
-  const keys = _.union(Object.keys(object1), Object.keys(object2));
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  const keys = _.union(object1Keys, object2Keys);
 
   const diff = keys.map((key) => {
     if (Object.hasOwn(object1, key) && !Object.hasOwn(object2, key)) {
