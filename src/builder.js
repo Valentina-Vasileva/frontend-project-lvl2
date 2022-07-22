dddb6dbe16124e60872dabd568464acb050356c1
Valentina <v.vasileva@nuzhnapomosh.ru>
2022-07-22 19:11:40 +0300

    add complex structure

--- src/builder.js
+++ src/builder.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 
-const createNode = (key, type, oldValue, newValue) => ({
+const createNode = (key, type, oldValue, newValue, children = null) => ({
   key,
   type,
   oldValue,
   newValue,
+  children
 });
 
 const buildDiff = (object1, object2) => {
@@ -14,18 +15,22 @@ const buildDiff = (object1, object2) => {
 
   const diff = keys.map((key) => {
     if (Object.hasOwn(object1, key) && !Object.hasOwn(object2, key)) {
-      return createNode(key, 'deleted', object1[key], null);
+      return createNode(key, 'deleted', object1[key], null, );
     }
 
     if (!Object.hasOwn(object1, key) && Object.hasOwn(object2, key)) {
       return createNode(key, 'added', null, object2[key]);
     }
 
-    if (object1[key] === object2[key]) {
-      return createNode(key, 'unchanged', object1[key], object2[key]);
+    if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+      return createNode(key, 'complex', object1[key], object2[key], buildDiff(object1[key], object2[key]));
     }
 
-    return createNode(key, 'changed', object1[key], object2[key]);
+    if (object1[key] !== object2[key]) {
+      return createNode(key, 'changed', object1[key], object2[key]);
+    }
+
+    return createNode(key, 'unchanged', object1[key], object2[key]);
   });
 
   return _.sortBy(diff, 'key');
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,24 +1,51 @@
-const formatToStylish = (data) => {
+import _ from "lodash";
+
+function format(data)
+{
+  return formatToStylish(data, 1);
+}
+
+const formatToStylish = (data, level) => {
+  const indent = ' '.repeat((level - 1) * 4);
   const formattedString = data.map((node) => {
-    const indent = ' '.repeat(2);
+    const formattedOldValue = stringify(node.oldValue, level);
+    const formattedNewValue = stringify(node.newValue, level);
 
     switch (node.type) {
       case 'added':
-        return `${indent}+ ${node.key}: ${node.newValue}`;
+        return `${indent}  + ${node.key}: ${formattedNewValue}`;
       case 'deleted':
-        return `${indent}- ${node.key}: ${node.oldValue}`;
+        return `${indent}  - ${node.key}: ${formattedOldValue}`;
       case 'changed': {
-        const deletedNode = `${indent}- ${node.key}: ${node.oldValue}`;
-        const addedNode = `${indent}+ ${node.key}: ${node.newValue}`;
+        const deletedNode = `${indent}  - ${node.key}: ${formattedOldValue}`;
+        const addedNode = `${indent}  + ${node.key}: ${formattedNewValue}`;
         return [deletedNode, addedNode].join('\n');
       }
+      case 'complex': {
+        const children = formatToStylish(node.children, level + 1);
+        return `${indent}    ${node.key}: ${children}`;
+      }
       case 'unchanged':
-        return `${indent}  ${node.key}: ${node.oldValue}`;
+        return `${indent}    ${node.key}: ${formattedOldValue}`;
       default:
         throw new Error(`The node type '${node.type}' is not identified`);
     }
   }).join('\n');
-  return `{\n${formattedString}\n}`;
+  return `{\n${formattedString}\n${indent}}`;
 };
 
-export default formatToStylish;
+const stringify = (value, level) => {
+  if (_.isObject(value)) {
+    const indent = " ".repeat(level * 4);
+    const keys = Object.keys(value);
+    const formattedValue = keys.map((key) => {
+      const childValue = stringify(value[key], level + 1);
+      return `${indent}    ${key}: ${childValue}`;
+    }).join("\n");
+    return `{\n${formattedValue}\n${indent}}`;
+  }
+
+  return value;
+};
+
+export default format;
